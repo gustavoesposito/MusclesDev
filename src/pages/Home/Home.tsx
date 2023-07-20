dd0ea5092b73aa5210ca37edaa425713adc1c12d
gustavoesposito <espositogustavo796@gmail.com>
2023-07-20 13:30:45 -0300

    Home view added

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,30 +1,123 @@
 import * as React from 'react';
-import Lottie from 'lottie-react-native';
-import { Container, ButtonWrapper } from './styles';
+import { useState } from 'react';
+import { Image } from 'react-native';
+import { ProgressBar } from 'react-native-paper';
+
+import {
+  Container,
+  HomeText,
+  HomeTextWrapper,
+  TouchScreenAction,
+  ImageSlideHolder,
+  ImageHolder,
+  OnboardingHolder,
+  ProgressBarWrapper,
+  ArrowHeaderWrapper,
+  SlideText,
+  SlideTextHolder,
+  StartButtonWrapper,
+} from './styles';
+
+import ArrowHeader from '../../components/ArrowHeader/ArrowHeader';
+import { Button } from '../../components/Button';
+
+import constants from '../../global/styles/constants';
 import { useNavigation } from '@react-navigation/native';
-import Button from '../../components/Button/Button';
 
 export const Home: React.FunctionComponent = () => {
+  const [isOnboarding, setIsOnboarding] = useState(false);
+  const [progress, setProgress] = useState(0.5);
+
   const navigation = useNavigation();
 
-  return (
+  function handleProgressUpdate() {
+    progress === 1 ? null : setProgress(progress + 0.5);
+    console.log('---Quando funciona---');
+  }
+
+  function ArrowBackHeader() {
+    return progress === 0.5 ? null : (
+      <ArrowHeaderWrapper>
+        <ArrowHeader
+          theme="light"
+          marginBottom={16}
+          marginLeft={16}
+          onPress={() => setProgress(progress - 0.5)}
+        />
+      </ArrowHeaderWrapper>
+    );
+  }
+
+  function renderImage() {
+    switch (progress) {
+      case 0.5:
+        return (
+          <>
+            <ImageSlideHolder>
+              <Image source={require('../../assets/work-out-sticker02.png')} />
+            </ImageSlideHolder>
+
+            <SlideTextHolder>
+              <SlideText>Action is the</SlideText>
+              <SlideText> key for all success</SlideText>
+            </SlideTextHolder>
+          </>
+        );
+      case 1:
+        return (
+          <>
+            <ImageSlideHolder>
+              <Image source={require('../../assets/work-out-sticker01.png')} />
+            </ImageSlideHolder>
+
+            <SlideTextHolder>
+              <SlideText>Work body and mind</SlideText>
+              <SlideText>take the success</SlideText>
+            </SlideTextHolder>
+
+            <StartButtonWrapper>
+              <Button.Small
+                iconName="chevron-right"
+                theme="success"
+                title={'Start now'}
+                onPress={() => navigation.navigate('PersonalChoose')}
+              />
+            </StartButtonWrapper>
+          </>
+        );
+      default:
+        return null;
+    }
+  }
+
+  return isOnboarding ? (
+    <Container>
+      <OnboardingHolder onPress={handleProgressUpdate}>
+        <ArrowBackHeader />
+
+        <ProgressBarWrapper marginTop={progress === 0.5 ? 42 : undefined}>
+          <ProgressBar
+            progress={progress}
+            color={constants.colors.green_success_100}
+          />
+        </ProgressBarWrapper>
+
+        {renderImage()}
+      </OnboardingHolder>
+    </Container>
+  ) : (
     <>
       <Container>
-        <Lottie
-          source={require('../../assets/animations/pencil-write.json')}
-          autoPlay
-          loop
-        />
+        <TouchScreenAction onPress={() => setIsOnboarding(true)}>
+          <HomeTextWrapper>
+            <ImageHolder>
+              <Image source={require('../../assets/dumbel-image.png')} />
+            </ImageHolder>
 
-        <ButtonWrapper>
-          <Button
-            iconName="chevron-right"
-            title={'Anote aqui!'}
-            onPress={() => {
-              navigation.navigate('Choosen');
-            }}
-          />
-        </ButtonWrapper>
+            <HomeText>DEV</HomeText>
+            <HomeText>MUSCLES</HomeText>
+          </HomeTextWrapper>
+        </TouchScreenAction>
       </Container>
     </>
   );
--- src/pages/Home/styles.tsx
+++ src/pages/Home/styles.tsx
@@ -1,29 +1,73 @@
 import styled from 'styled-components/native';
 
+interface HomeProps {
+  marginTop?: number;
+  marginBottom?: number;
+  marginRight?: number;
+  marginLeft?: number;
+}
+
 export const Container = styled.View`
   flex: 1;
-  background-color: ${({ theme }) => theme.colors.neutral_00};
+  background-color: ${({ theme }) => theme.colors.dark_gray50};
 `;
 
-export const Header = styled.View`
+export const TouchScreenAction = styled.TouchableOpacity`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.dark_gray50};
+`;
+
+export const HomeText = styled.Text`
+  font-size: 50px;
+  margin-top: 16px;
+  font-family: ${({ theme }) => theme.fonts.rubik_regular};
+  color: ${({ theme }) => theme.colors.green_success_100};
+`;
+
+export const HomeTextWrapper = styled.View`
+  flex: 1;
+  align-items: center;
+  margin: 0 32px;
+  justify-content: center;
+`;
+
+export const ImageHolder = styled.View`
+  margin-bottom: 16px;
+  align-items: center;
+`;
+
+export const ImageSlideHolder = styled.View`
+  flex: 2;
   align-items: center;
   justify-content: center;
-  margin-top: 30px;
-  padding-bottom: 00px;
 `;
 
-export const ButtonWrapper = styled.View`
-  flex:1
-  width: 80%;
-  justify-content: center;
+export const OnboardingHolder = styled.TouchableOpacity`
+  flex: 1;
+  margin-top: 45px;
+`;
+
+export const ProgressBarWrapper = styled.View<HomeProps>`
+  ${({ marginTop }) => marginTop && `margin-top: ${marginTop}px;`}
+  ${({ marginRight }) => marginRight && `margin-right: ${marginRight}px;`}
+  ${({ marginBottom }) => marginBottom && `margin-bottom: ${marginBottom}px;`}
+  ${({ marginLeft }) => marginLeft && `margin-left: ${marginLeft}px;`}
+`;
+
+export const ArrowHeaderWrapper = styled.View``;
+
+export const SlideText = styled.Text`
+  font-size: 32px;
+  color: ${({ theme }) => theme.colors.white};
+  font-family: ${({ theme }) => theme.fonts.lexend_medium};
+`;
+
+export const SlideTextHolder = styled.View`
+  flex: 1;
   align-items: center;
-  position:relative
-  margin: 300px 40px 15px
 `;
 
-export const Title = styled.Text`
-  font-size: 40px;
-  font-weight: bold;
-  margin-top: 90px;
-  color: ${({ theme }) => theme.colors.dark};
+export const StartButtonWrapper = styled.View`
+  flex: 1;
+  align-items: center;
 `;
