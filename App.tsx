1dcdec3058df109666dddaf92c297125e50f96bb
gustavoesposito <espositogustavo796@gmail.com>
2023-07-20 13:30:05 -0300

    The app commit

--- App.tsx
+++ App.tsx
@@ -8,6 +8,15 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto';
+
+import { BebasNeue_400Regular } from '@expo-google-fonts/bebas-neue';
+import {
+  Lexend_700Bold,
+  Lexend_600SemiBold,
+  Lexend_500Medium,
+  Lexend_400Regular,
+} from '@expo-google-fonts/lexend';
+import { RubikMonoOne_400Regular } from '@expo-google-fonts/rubik-mono-one';
 import AppLoading from 'expo-app-loading';
 import Routes from './src/routes';
 
@@ -15,6 +24,12 @@ const App: React.FunctionComponent = () => {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
+    BebasNeue_400Regular,
+    RubikMonoOne_400Regular,
+    Lexend_700Bold,
+    Lexend_600SemiBold,
+    Lexend_500Medium,
+    Lexend_400Regular,
   });
 
   if (!fontsLoaded) {
